7f69388eb99a1646c85dd5f636eda18fb6d8aa5d
Jessi <jessivelazq1@gmail.com>
2021-08-14 12:37:32 -0700

    gen intro and assignment 01

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Learn Vue.js!',
+      courseGoalA: 'Learn Vue.js Really Well',
+      courseGoalB: 'Learn Vue.js Super Well',
       vueLink: 'https://vuejs.org/'
+    }
+  },
+  methods: {
+    outputGoal() {
+      const rand = Math.random();
+      if (rand < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
     }
   }
 });
